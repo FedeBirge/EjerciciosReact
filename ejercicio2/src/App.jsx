83514c8e68ed9fa308dee761f40e466af6428c47
FedeBirge <fedeikky@gmail.com>
2023-07-17 10:43:30 -0300

    eje 2

---
+++ ejercicio2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Main />
+      <Footer />
+      
+    </div>
+  )
+}
+
+export default App
